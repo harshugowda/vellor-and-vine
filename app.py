870ef3483ae765e2a7d663bcc57283dfcc49f964
harshugowda <hharshitha2001@gmail.com>
2026-08-12 14:04:49 +0530

    Disable Flask debug mode

--- app.py
+++ app.py
@@ -1193,6 +1193,9 @@ def notification_count():
 # Run App
 # -----------------------------
 if __name__ == "__main__":
+
     with app.app_context():
+
         db.create_all()
-    app.run(debug=True)
+
+    app.run()
